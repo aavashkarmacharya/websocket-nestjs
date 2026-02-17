5a4872467aff47db44dfccf1163805d101c0b6b0
Aavash Karmacharya <karmacharya.aavash0@gmail.com>
2026-02-17 10:27:20 +0545

    added basic chat system (super rusty) and basic room implementation (broken)

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,14 +1,46 @@
+import { OnModuleInit } from '@nestjs/common';
 import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-
+import { Client } from 'node_modules/socket.io/dist/client';
+import { stringify } from 'querystring';
+import { Server, Socket } from 'socket.io';
 @WebSocketGateway()
-export class ChatGateway {
+export class chatgateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+  onModuleInit() {
+    this.server.on('connection', (Socket) => {
+      console.log(`Connection established! socket id: ${Socket.id}`);
+    });
+  }
+
+  @SubscribeMessage('joinroom')
+  handlejoinroom(Client: Socket, room: string) {
+    Client.join(room);
+    Client.emit(`${Client.id} has joined the room!`);
+  }
+  @SubscribeMessage('sendmessage')
+  handlesendmessage(client: Socket, message: string, room: string) {
+    this.server.to(room).emit(message);
+  }
+
   @SubscribeMessage('message')
-  async HandleMessage(@MessageBody() message: string): Promise<string> {
-    console.log(message);
-    return message;
+  handlemessage(@MessageBody() message: string) {
+    const data: string = message.toLocaleLowerCase();
+    const val: string = 'hey';
+    this.server.emit('message', {
+      content: message,
+    });
+  }
+
+  @SubscribeMessage('responder')
+  handleresponder(@MessageBody() data: string) {
+    this.server.emit('responder', {
+      content: data,
+    });
   }
 }
